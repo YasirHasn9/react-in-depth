0edfe8970c5de8612a0190f3741bc19eceedb859
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-10 11:38:47 -0800

    delete unused files

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,102 +1,21 @@
 // ts project 
 import {Route , Link} from "react-router-dom"
-import {ThemeProvider,DefaultTheme}  from "styled-components"
+import {ThemeProvider}  from "styled-components"
 import {lightTheme} from "./theme"
 import {NavBar} from "./style"
 import "./App.css"
 
-// import GrandFather from "./passingDrop"
-// import CountChanger from "./CountChanger"
-// import TestEffects from "./sideEffects"
-// import CleanUp from "./cleanup"
-// import DogApp from "./effects/Dog"
-// import {useState, useEffect} from "react"
-// import NewsFeed  from "./effects/newsFeed"
-// import Name from "./propsChecking/name"
-// import Lifecycle from "./Lifecycle/index"
-// import ChangeName from "./Lifecycle/example"
-// import ListCities from "./Lifecycle/ListCities"
 
-// import DisplayWrappedComponentsByHOC from "./HOC/index"
-// import ManageState from "./hooks/index"
-// import DifferentBehaviors from "./Lifecycle/checkBehavior"
-// import {MemoFun} from "./hooks/memo"
-
-// This is a React components
-// Component must be capitalized 
-// it should return a react element 
-
-
-import {TextInsideTheLink} from "./style"
-export type StyleProps = {
-    color:string;
-  }
-function App():JSX.Element{
-  // // if console log what something gives us it will return an array of state and setter
-  // const something = useState(false) 
-  // so we can get the state and the setter
-  // const state = something[0]
-  // const fun = something[1]
-  // we can achieve the same result by writing only one line
-
-  // const [state , setState] = useState(true)
-  // const [count , setCount] = useState(0)
-
-  // this is for the purpose of props-type
-  // const [name , setName] = useState("")
-  // const handleChange = e => setName(e.target.value)
-
-  // this is a powerful feature react has 
-  // whenever the state changes react knows what to do and renders on the dom
-  
-  // const handleAdding = () => {
-  //   setCount(() => count + 1)
-  // }
-  // const multi = () => {
-  //   setCount(() => count * 5)
-  // }
-
-
-  // const reset = () => {
-  //   setCount(0)
-  // }
+const App: React.FC  = ():JSX.Element => {
+ 
   return (
     <ThemeProvider theme={lightTheme}>
     <div className="App">
-     
-        {/* <p>Count: {count}</p>
-        we can make the button in separate component 
-        and also we can named CountChanger
-        <button onClick={handleAdding}>Add</button>
-        <button onClick={multi}>Multiply By 5</button>
-        <button onClick={reset}>Reset</button>
-        <CountChanger changeCounter={handleAdding} label="Increase"/>
-        <CountChanger changeCounter={multi} label="Multiply"/>
-        <CountChanger changeCounter={reset} label="Reset"/>
-
-        <TestEffects />
-
-        <CleanUp />
-      <GrandFather />
-      <DogApp />
-      <NewsFeed />
-      <Name name={name} handleChange={handleChange} />
-      <Lifecycle />
-      <ChangeName />
-      <h1>Test </h1>
-      <Lifecycle />
-  
-
-      <ListCities /> */}
       <header className="App-header">
         <NavBar>
           <h1><Link to="/">Tinkers</Link></h1>
           <div>
-            <Link to="/">
-              <TextInsideTheLink color="red">
-
-              </TextInsideTheLink>
-            </Link>
+            <Link to="/">home</Link>
             <Link to="/shop">shop</Link>
           </div>
         </NavBar>
--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -2,17 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router } from "react-router-dom"
-import './index.css';
 import App from './App';
 
 
 
-
-
-
-// all the react element will be injected in this root element.
-// we can call the anchor point
-// the down code will be done once per app but most of the work would be in the app in many many components 
 const rootElement = document.getElementById("root")
 ReactDOM.render(
 <Router>
