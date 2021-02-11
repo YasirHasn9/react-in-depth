e127540c9d5510c2e3352bca2d05e6e11e376948
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-10 22:01:20 -0800

    cleaned it

--- react-app/src/TSproject/home/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-import HomeBk  from "../assets/test.webp"
-export const Wrapper = styled.div`
-    background-image:url(${HomeBk});
-    background-repeat:no-repeat;
-    background-position:top;
-    height:40rem;
-    width:100vw;
-`
