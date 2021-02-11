9ddf46a17abd7c5ea6e7ff3e261044e18513fbb0
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-11 08:42:31 -0800

    create a home page

---
+++ theories/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react"
+import {IPage} from "../interfaces/page"
+import {IRoute} from "../interfaces/route"
+import {logging} from "../config/logging"
+
+
+const Home : React.FC = (props) => {
+    useEffect(() => {
+        logging.info(`loading ${props.name}`)
+    } , [ ])
+    return <p>This is the home page</p>
+}
+
+export default Home
