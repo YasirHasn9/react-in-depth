8a8ef534bf2fb8c0556ec7b588ee96acdb5b896f
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-10 22:36:37 -0800

    setup new project with ts

---
+++ theories/src/application.tsx
@@ -0,0 +1,12 @@
+// the purpose of this file is to the backbone of our project
+// here, we are gonna load the initial state 
+import React from "react"
+
+
+const Application: React.FC<any> = () => {
+    return (
+        <div>Hello</div>
+    )
+}
+
+export default Application
