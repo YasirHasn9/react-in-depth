f458862070327ae4a2132b9bee80b8d528ca79d5
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-11 08:21:29 -0800

    logging

--- theories/src/config/logging.ts
+++ theories/src/config/logging.ts
@@ -2,6 +2,8 @@ import {config} from "./config"
 
 const DEFAULT_NAMESPACE =  config.default.namespace
 
+
+// here the user allows to pass a message and a namespace. The namespace tells where the message coming form !
 const info = (message:any , namespace?:string) => {
     if(typeof message === "string"){
         console.log(`[${getDate()}] [${namespace || DEFAULT_NAMESPACE}] [INFO] ${message}`)
