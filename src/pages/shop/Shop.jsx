573497af5d2edb7b7daa998acbaaf8e5c28d2189
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-17 13:10:09 -0800

    display products

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,9 +1,18 @@
-
-
-
-export function Shop(props){
-    console.log("this shop",props)
+export function Shop({products}){
     return (
-        <h1>Shop</h1>
+        <>
+        <div>
+            wrapper
+            {
+                products.map(product => (
+                    <div key={product.id}>
+                        <h3>{product.title}</h3>
+                        <img src={product.image} alt={product.title} />
+                        <p>{product.price}</p>
+                    </div>
+                ))
+            }
+        </div>
+        </>
     )
 }
