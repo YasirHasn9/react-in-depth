8fc4d6d3f724f20329118c80d0cecddc64bb52c6
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-18 20:31:13 -0800

    clean the Home Comp

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,31 +1,17 @@
 
-import {Route , Link, useRouteMatch} from "react-router-dom"
+import {Route ,Switch, Link} from "react-router-dom"
 // components
 import {Navigation} from "./nav/Navigation"
-import {Shop} from "../shop/Shop"
-import {Product} from "../product/Product"
 
 
 // style
 import {Wrapper} from "./style.home"
 
 export const Home = (props) => {
-    const {path} = useRouteMatch()
-    console.log("This is from the home",path)
     return(
         <Wrapper>
             <div className="overlay">
            <Navigation />
-
-            <div>
-                <Route
-                exact
-                 path={`/shop`} 
-                 render={propsRoute => <Shop {...propsRoute} products={props.products} />} />
-                <Route 
-                path={`/shop/:id`} 
-                render={propsRoute => <Product {...propsRoute} products={props.products} />}  />
-            </div>
             </div>
         </Wrapper>
     )
