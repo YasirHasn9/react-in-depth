53876071bbe05279719268542b411b51bb83f0d4
Yasir Hassan <hasanyasir01@gmail.com>
2021-03-04 13:01:56 -0800

    difference between useState and useReducer

--- src/testBootstrap/index.js
+++ src/testBootstrap/index.js
@@ -1,14 +1,13 @@
+import {Counter} from "../reducer/index"
 import {Container , Row , Col} from "react-bootstrap"
 import "./style.css"
 
 export const PlayingWithBootstrap = () => {
     return (
-        <Container fluid="md" className="test">
+        <Container fluid="md">
             <h1>Contaier</h1>
             <Row>
-                <Col>1</Col>
-                <Col>2</Col>
-                <Col>3</Col>
+                <Counter />
             </Row>
         </Container>
     )
