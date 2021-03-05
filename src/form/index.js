93e582875e7527421af69c1bd597387d825afc10
Yasir Hassan <hasanyasir01@gmail.com>
2021-03-04 16:25:37 -0800

    change input values dynamically

--- src/form/index.js
+++ src/form/index.js
@@ -1,26 +1,67 @@
 // form in react and how to handle it 
 // form contains inputs and labels, so we can get an information form the customer
-import {Container} from "react-bootstrap"
+import { useState } from "react"
+import {Container , Row} from "react-bootstrap"
 
 
 export const ReactFrom = () => {
+    const [fname , setName] = useState("")
+    const [fav_veh , setFavVeh] = useState("")
+    const [isHappy, setIsHappy] = useState(false)
     return (
         <Container style={{padding:"200px", background:"#eee"}}>
+            <Row>
+                <p>Name: <h3>{fname ? fname : "Unknown"}</h3></p>
+            </Row>
+
+            <Row>
+                <p>Fav Vehicle: {fav_veh ? fav_veh : "no idea"}</p>
+            </Row>
+
+            <Row>
+                <p>The mood is {isHappy ? "Happy" : "unHappy"}</p>
+            </Row>
+
+
         <form>
-            <label>name</label>
-            <input type="text" name="name" />
+            <label htmlFor="fname">name</label>
+
+
+
+            <input onChange={e => {
+                setName(e.target.value)
+            }} type="text" name="fname" />
             <br />
-            <label htmlFor="fav_vehicle" >Fav Vehicle</label> 
-            <select id="fav_vehicle" name="fav_vehicle">
-                <option value={1}>truck</option>
-                <option value={2}>car</option>
-                <option value={3}>airplane</option>
+
+
+
+
+            <label  htmlFor="fav_vehicle" >Fav Vehicle</label> 
+
+
+
+            <select onChange={e => {
+                setFavVeh(e.target.value)
+            }} id="fav_vehicle" name="fav_vehicle">
+                <option>truck</option>
+                <option>car</option>
+                <option>airplane</option>
             </select>
+
+
+
+
             <br />
             <label htmlFor="isHappy">Are you happy</label>
-            <input name="isHappy" type="checkbox" id="isHappy" />
+            <input 
+            onChange={e => setIsHappy(e.target.checked)} 
+            name="isHappy" type="checkbox"
+            id="isHappy"
+            value={isHappy}
+             />
             <br />
             <input type="submit" />
         </form>
         </Container>
     )
+}
